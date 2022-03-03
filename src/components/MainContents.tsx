47eb06127ad5a95a84c0bdab4935f124d1999f9e
Owen Dearman <owendearman@gmail.com>
2022-03-03 12:23:25 +0000

    fixed Place Item 1

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -4,7 +4,7 @@ import PlaceItem from "./PlaceItem";
 function MainContents(): JSX.Element {
   return (
     <PlaceItem
-      title={"Limestone Rocks"}
+      title={"Limestone Rocks!"}
       name={"Malham Cove"}
       country={"United Kingdom"}
       timesVisited={2}
@@ -14,7 +14,7 @@ function MainContents(): JSX.Element {
       ]}
       locationLink={"https://goo.gl/maps/aaQanD5HBcKgGhFG8"}
       description={
-        "This massive limestone formation was created by glacial flow thousands of years ago. I love it because it's so peaceful yet is also a reminder of how destructive but beautiful nature can be."
+        "This immense limestone formation was created by glacial flow thousands of years ago. I love it because it's so peaceful, being surrounded by miles and miles of countryside. It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like a throne for a giant."
       }
     />
   );
