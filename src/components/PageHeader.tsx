8dee8b163c6ae20ee950890c602c84a7d49e2a39
Owen Dearman <owendearman@gmail.com>
2022-03-03 13:00:28 +0000

    typo/format/lint

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,11 +1,11 @@
 //This component concerns the page layout for the header
 
 function PageHeader(): JSX.Element {
-  return(
-      <>
-        <h1 className="pageTitle"> Owen's Top 5 Places</h1>
-      </>
-  ); 
+  return (
+    <>
+      <h1 className="PageTitle"> Owen's Top 5 Places</h1>
+    </>
+  );
 }
 
 export default PageHeader;
