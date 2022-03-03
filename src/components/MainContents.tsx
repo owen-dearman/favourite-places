d97edae0b00564a15ddf2283ccfadbbd5b13ab96
Owen Dearman <owendearman@gmail.com>
2022-03-03 13:35:44 +0000

    updated descriptions

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -14,8 +14,12 @@ function MainContents(): JSX.Element {
           "Malham Cove formation in the background with a stream in the foreground and trees on either side",
         ]}
         locationLink={"https://goo.gl/maps/aaQanD5HBcKgGhFG8"}
-        description={
-          "This immense limestone formation was created by glacial flow thousands of years ago. I love it because it's so peaceful, being surrounded by miles and miles of countryside. It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like a throne for a giant."
+        description={`
+          This immense limestone formation was created by glacial flow more than 12 thousand years ago.
+          It stands at 80 metres above the ground and features the famous limestone pavement at the top (Harry Potter and the Deathly Hallows pt.1).
+          I love it because it's so peaceful, being surrounded by miles and miles of countryside - you can hear the babbling stream and peregrine falcons in the cliff face. 
+          It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like you're staring at a throne for a giant.
+        `
         }
       />
       <PlaceItem
@@ -25,8 +29,11 @@ function MainContents(): JSX.Element {
         timesVisited={100}
         placeImage={["/images/york.jpg", "York Shambles"]}
         locationLink={"https://goo.gl/maps/9Arjyy6Ka8uzCvsg8"}
-        description={
-          "I spent 4 years in York at university and it quickly rose to become one of my favourite places, not least because it's where I met my girlfriend. It ouses (wink wink) culture and friendliness that I've yet to experience elsewhere"
+        description={`
+          I spent 4 years in York at university and it quickly rose to become one of my favourite places, not least because it's where I met my girlfriend.
+          It ouses (wink wink) culture and friendliness that I've yet to experience elsewhere. There's so many places to see: The Shambles, Ouse, Minster, Jorvik Centre, Railway Museum, and Castle to name a few.
+          Plus, there's over 365 pubs and a multitude of independent cafes, restuarants and shops.
+        `
         }
       />
       <PlaceItem
@@ -39,12 +46,16 @@ function MainContents(): JSX.Element {
           "Cheslyn house situated in its grounds",
         ]}
         locationLink={"https://goo.gl/maps/HzhdzLHXLHcWUe239"}
-        description={
-          "This hidden oasis of lawn, trees and flowers is one of, if not the best hidden treasure of Watford. With a fish pond and aviary, it's a secluded place to wander and get lost in your thoughts"
+        description={`
+          This hidden oasis is one of, if not the best hidden treasure of Watford. 
+          The 3.5 acre gardens have space to sit and reflect, or explore an expanse of forest, or admire the rare flowers.
+          With a fish pond and aviary, it's a secluded place to wander and get lost in your thoughts. 
+          I used to go here with my Nan and so it holds special memories for me.
+        `
         }
       />
       <PlaceItem
-        title={"> Dubrovnik"}
+        title={"The CROAT (think GOAT) of Croatia"}
         name={"Porec"}
         country={"Croatia"}
         timesVisited={1}
@@ -53,8 +64,11 @@ function MainContents(): JSX.Element {
           "The peninsula of Porec with Meditteranean houses and clear blue sea",
         ]}
         locationLink={"https://goo.gl/maps/6hY2uErzHpnRrJf88"}
-        description={
-          "This represents my favourite family holiday, with sun, sea and the most beautiful architecture I've ever seen. Take me back!"
+        description={`
+          "This represents my favourite family holiday, with sun, sea and the most beautiful architecture I've ever seen.
+          With easy access to Rovinj (the city on the hill) or Pula with its Roman amphitheatre, there's places to go once you've explored Porec.
+          Take me back!
+          `
         }
       />
       <PlaceItem
@@ -67,8 +81,11 @@ function MainContents(): JSX.Element {
           "The boat house infront of the middle lake at sunset",
         ]}
         locationLink={"https://goo.gl/maps/9Ju3r6ZMMAmQX25d9"}
-        description={
-          "Kayaking, barbeques, swimming and walking - in this park you can do it all. With three mahoosive lakes connected by tunnels, there is endless potential for exploration."
+        description={`
+          Kayaking, barbeques, swimming and walking - in this park you can do it all. 
+          With three mahoosive lakes connected by tunnels, there is endless potential for exploration.
+          It would take hundreds of visits to see everything and cover every inch of this classic American park.
+        `
         }
       />
     </section>
