bddec8c441bd55fa3f88bd31f13c884129af4c04
Owen Dearman <owendearman@gmail.com>
2022-03-03 13:39:42 +0000

    updated decsriptions

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -19,8 +19,7 @@ function MainContents(): JSX.Element {
           It stands at 80 metres above the ground and features the famous limestone pavement at the top (Harry Potter and the Deathly Hallows pt.1).
           I love it because it's so peaceful, being surrounded by miles and miles of countryside - you can hear the babbling stream and peregrine falcons in the cliff face. 
           It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like you're staring at a throne for a giant.
-        `
-        }
+        `}
       />
       <PlaceItem
         title={"Bowled a Yorker"}
@@ -33,8 +32,7 @@ function MainContents(): JSX.Element {
           I spent 4 years in York at university and it quickly rose to become one of my favourite places, not least because it's where I met my girlfriend.
           It ouses (wink wink) culture and friendliness that I've yet to experience elsewhere. There's so many places to see: The Shambles, Ouse, Minster, Jorvik Centre, Railway Museum, and Castle to name a few.
           Plus, there's over 365 pubs and a multitude of independent cafes, restuarants and shops.
-        `
-        }
+        `}
       />
       <PlaceItem
         title={"The Secret Garden"}
@@ -51,8 +49,7 @@ function MainContents(): JSX.Element {
           The 3.5 acre gardens have space to sit and reflect, or explore an expanse of forest, or admire the rare flowers.
           With a fish pond and aviary, it's a secluded place to wander and get lost in your thoughts. 
           I used to go here with my Nan and so it holds special memories for me.
-        `
-        }
+        `}
       />
       <PlaceItem
         title={"The CROAT (think GOAT) of Croatia"}
@@ -68,8 +65,7 @@ function MainContents(): JSX.Element {
           "This represents my favourite family holiday, with sun, sea and the most beautiful architecture I've ever seen.
           With easy access to Rovinj (the city on the hill) or Pula with its Roman amphitheatre, there's places to go once you've explored Porec.
           Take me back!
-          `
-        }
+          `}
       />
       <PlaceItem
         title={"Say it like you're sneezing"}
@@ -85,8 +81,7 @@ function MainContents(): JSX.Element {
           Kayaking, barbeques, swimming and walking - in this park you can do it all. 
           With three mahoosive lakes connected by tunnels, there is endless potential for exploration.
           It would take hundreds of visits to see everything and cover every inch of this classic American park.
-        `
-        }
+        `}
       />
     </section>
   );
