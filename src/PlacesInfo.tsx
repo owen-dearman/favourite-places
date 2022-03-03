d6233f806ab6f933bbb9430c67854e4807d343ca
Owen Dearman <owendearman@gmail.com>
2022-03-03 18:33:27 +0000

    lint

--- src/PlacesInfo.tsx
+++ src/PlacesInfo.tsx
@@ -1,88 +1,99 @@
 interface placesInfoInterface {
-    title: string;
-    name: string;
-    country: string;
-    timesVisited: number;
-    placeImage: string[];
-    locationLink: string;
-    description: string;
-    linkID: string;
-  }
+  title: string;
+  name: string;
+  country: string;
+  timesVisited: number;
+  placeImage: string[];
+  locationLink: string;
+  description: string;
+  linkID: string;
+}
 
-let PlacesInfo = [
-    {
-      title: "Limestone Rocks", 
-      name: "Malham Cove", 
-      country: "United Kingdom", 
-      timesVisited: 2, 
-      placeImage: ["/images/malhamCove.jpg", "Malham Cove formation in the background with a stream in the foreground and trees on either side"],
-      locationLink: "https://goo.gl/maps/aaQanD5HBcKgGhFG8",
-      linkID: "malhamCove",
-      description: `
+const PlacesInfo = [
+  {
+    title: "Limestone Rocks",
+    name: "Malham Cove",
+    country: "United Kingdom",
+    timesVisited: 2,
+    placeImage: [
+      "/images/malhamCove.jpg",
+      "Malham Cove formation in the background with a stream in the foreground and trees on either side",
+    ],
+    locationLink: "https://goo.gl/maps/aaQanD5HBcKgGhFG8",
+    linkID: "malhamCove",
+    description: `
       This immense limestone formation was created by glacial flow more than 12 thousand years ago.
       It stands at 80 metres above the ground and features the famous limestone pavement at the top (Harry Potter and the Deathly Hallows pt.1).
       I love it because it's so peaceful, being surrounded by miles and miles of countryside - you can hear the babbling stream and peregrine falcons in the cliff face. 
       It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like you're staring at a throne for a giant.
-      `
-    },
-    {
-      title: "Bowled a Yorker",
-      name: "York",
-      country: "United Kingdom",
-      timesVisited: 100,
-      placeImage: ["/images/york.jpg", "York Shambles"],
-      locationLink: "https://goo.gl/maps/9Arjyy6Ka8uzCvsg8",
-      linkID: "york",
-      decsription: `
+      `,
+  },
+  {
+    title: "Bowled a Yorker",
+    name: "York",
+    country: "United Kingdom",
+    timesVisited: 100,
+    placeImage: ["/images/york.jpg", "York Shambles"],
+    locationLink: "https://goo.gl/maps/9Arjyy6Ka8uzCvsg8",
+    linkID: "york",
+    decsription: `
       I spent 4 years in York at university and it quickly rose to become one of my favourite places, not least because it's where I met my girlfriend.
       It ouses (wink wink) culture and friendliness that I've yet to experience elsewhere. There's so many places to see: The Shambles, Ouse, Minster, Jorvik Centre, Railway Museum, and Castle to name a few.
       Plus, there's over 365 pubs and a multitude of independent cafes, restuarants and shops.
-    `
-    },
-    {
-      title: "The Secret Garden",
-      name: "Cheslyn Gardens",
-      country: "United Kingdom",
-      timesVisited: 10,
-      placeImage: ["/images/cheslyn.jpeg","Cheslyn house situated in its grounds",],
-      locationLink: "https://goo.gl/maps/HzhdzLHXLHcWUe239",
-      linkID: "cheslyn",
-      decsription: `
+    `,
+  },
+  {
+    title: "The Secret Garden",
+    name: "Cheslyn Gardens",
+    country: "United Kingdom",
+    timesVisited: 10,
+    placeImage: [
+      "/images/cheslyn.jpeg",
+      "Cheslyn house situated in its grounds",
+    ],
+    locationLink: "https://goo.gl/maps/HzhdzLHXLHcWUe239",
+    linkID: "cheslyn",
+    decsription: `
       This hidden oasis is one of, if not the best hidden treasure of Watford. 
       The 3.5 acre gardens have space to sit and reflect, or explore an expanse of forest, or admire the rare flowers.
       With a fish pond and aviary, it's a secluded place to wander and get lost in your thoughts. 
       I used to go here with my Nan and so it holds special memories for me.
-    `
-    },
-    {
-      title: "The CROAT (think GOAT) of Croatia",
-      name: "Porec",
-      country: "Croatia",
-      timesVisited: 1,
-      placeImage: ["/images/porec.jpg","The peninsula of Porec with Meditteranean houses and clear blue sea"],
-      locationLink: "https://goo.gl/maps/6hY2uErzHpnRrJf88",
-      linkID: "porec",
-      decsription: `
+    `,
+  },
+  {
+    title: "The CROAT (think GOAT) of Croatia",
+    name: "Porec",
+    country: "Croatia",
+    timesVisited: 1,
+    placeImage: [
+      "/images/porec.jpg",
+      "The peninsula of Porec with Meditteranean houses and clear blue sea",
+    ],
+    locationLink: "https://goo.gl/maps/6hY2uErzHpnRrJf88",
+    linkID: "porec",
+    decsription: `
       "This represents my favourite family holiday, with sun, sea and the most beautiful architecture I've ever seen.
       With easy access to Rovinj (the city on the hill) or Pula with its Roman amphitheatre, there's places to go once you've explored Porec.
       Take me back!
-      `
-    },
-    {
-      title: "Say it like you're sneezing",
-      name: "Cochituate State Park",
-      country: "USA",
-      timesVisited: 4,
-      placeImage: ["/images/cochituate.jpeg","The boat house infront of the middle lake at sunset"],
-      locationLink: "https://goo.gl/maps/9Ju3r6ZMMAmQX25d9",
-      linkID: "coch",
-      decsription: `
+      `,
+  },
+  {
+    title: "Say it like you're sneezing",
+    name: "Cochituate State Park",
+    country: "USA",
+    timesVisited: 4,
+    placeImage: [
+      "/images/cochituate.jpeg",
+      "The boat house infront of the middle lake at sunset",
+    ],
+    locationLink: "https://goo.gl/maps/9Ju3r6ZMMAmQX25d9",
+    linkID: "coch",
+    decsription: `
       Kayaking, barbeques, swimming and walking - in this park you can do it all. 
       With three mahoosive lakes connected by tunnels, there is endless potential for exploration.
       It would take hundreds of visits to see everything and cover every inch of this classic American park.
-    `
-    },
-  ]
+    `,
+  },
+];
 
-  export default PlacesInfo
- 
+export default PlacesInfo;
--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -13,13 +13,16 @@ interface placesInfoInterface {
 }
 
 function MainContents(): JSX.Element {
-  let PlacesInfo: placesInfoInterface[] = [
+  const PlacesInfo: placesInfoInterface[] = [
     {
-      title: "Limestone Rocks", 
-      name: "Malham Cove", 
-      country: "United Kingdom", 
-      timesVisited: 2, 
-      placeImage: ["/images/malhamCove.jpg", "Malham Cove formation in the background with a stream in the foreground and trees on either side"],
+      title: "Limestone Rocks",
+      name: "Malham Cove",
+      country: "United Kingdom",
+      timesVisited: 2,
+      placeImage: [
+        "/images/malhamCove.jpg",
+        "Malham Cove formation in the background with a stream in the foreground and trees on either side",
+      ],
       locationLink: "https://goo.gl/maps/aaQanD5HBcKgGhFG8",
       linkID: "malhamCove",
       description: `
@@ -27,7 +30,7 @@ function MainContents(): JSX.Element {
       It stands at 80 metres above the ground and features the famous limestone pavement at the top (Harry Potter and the Deathly Hallows pt.1).
       I love it because it's so peaceful, being surrounded by miles and miles of countryside - you can hear the babbling stream and peregrine falcons in the cliff face. 
       It is also a reminder of how destructive yet beautiful nature can be. When sitting at the base, it feels like you're staring at a throne for a giant.
-      `
+      `,
     },
     {
       title: "Bowled a Yorker",
@@ -41,14 +44,17 @@ function MainContents(): JSX.Element {
       I spent 4 years in York at university and it quickly rose to become one of my favourite places, not least because it's where I met my girlfriend.
       It ouses (wink wink) culture and friendliness that I've yet to experience elsewhere. There's so many places to see: The Shambles, Ouse, Minster, Jorvik Centre, Railway Museum, and Castle to name a few.
       Plus, there's over 365 pubs and a multitude of independent cafes, restuarants and shops.
-    `
+    `,
     },
     {
       title: "The Secret Garden",
       name: "Cheslyn Gardens",
       country: "United Kingdom",
       timesVisited: 10,
-      placeImage: ["/images/cheslyn.jpeg","Cheslyn house situated in its grounds",],
+      placeImage: [
+        "/images/cheslyn.jpeg",
+        "Cheslyn house situated in its grounds",
+      ],
       locationLink: "https://goo.gl/maps/HzhdzLHXLHcWUe239",
       linkID: "cheslyn",
       description: `
@@ -56,42 +62,48 @@ function MainContents(): JSX.Element {
       The 3.5 acre gardens have space to sit and reflect, or explore an expanse of forest, or admire the rare flowers.
       With a fish pond and aviary, it's a secluded place to wander and get lost in your thoughts. 
       I used to go here with my Nan and so it holds special memories for me.
-    `
+    `,
     },
     {
       title: "The CROAT (think GOAT) of Croatia",
       name: "Porec",
       country: "Croatia",
       timesVisited: 1,
-      placeImage: ["/images/porec.jpg","The peninsula of Porec with Meditteranean houses and clear blue sea"],
+      placeImage: [
+        "/images/porec.jpg",
+        "The peninsula of Porec with Meditteranean houses and clear blue sea",
+      ],
       locationLink: "https://goo.gl/maps/6hY2uErzHpnRrJf88",
       linkID: "porec",
       description: `
       "This represents my favourite family holiday, with sun, sea and the most beautiful architecture I've ever seen.
       With easy access to Rovinj (the city on the hill) or Pula with its Roman amphitheatre, there's places to go once you've explored Porec.
       Take me back!
-      `
+      `,
     },
     {
       title: "Say it like you're sneezing",
       name: "Cochituate State Park",
       country: "USA",
       timesVisited: 4,
-      placeImage: ["/images/cochituate.jpeg","The boat house infront of the middle lake at sunset"],
+      placeImage: [
+        "/images/cochituate.jpeg",
+        "The boat house infront of the middle lake at sunset",
+      ],
       locationLink: "https://goo.gl/maps/9Ju3r6ZMMAmQX25d9",
       linkID: "coch",
       description: `
       Kayaking, barbeques, swimming and walking - in this park you can do it all. 
       With three mahoosive lakes connected by tunnels, there is endless potential for exploration.
       It would take hundreds of visits to see everything and cover every inch of this classic American park.
-    `
+    `,
     },
-  ]
-    let placesArray = PlacesInfo.map(contentBuilder);
-    return <>{placesArray}</>
+  ];
+  const placesArray = PlacesInfo.map(contentBuilder);
+  return <>{placesArray}</>;
 }
 
-function contentBuilder(place: placesInfoInterface): JSX.Element{
+function contentBuilder(place: placesInfoInterface): JSX.Element {
   return (
     <PlaceItem
       title={place.title}
@@ -101,10 +113,9 @@ function contentBuilder(place: placesInfoInterface): JSX.Element{
       placeImage={place.placeImage}
       locationLink={place.locationLink}
       linkID={place.linkID}
-      description={place.description}   
+      description={place.description}
     />
-  )
+  );
 }
 
 export default MainContents;
-
