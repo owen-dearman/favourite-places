bafca67d636bb65b224ca4394d315484387c029f
Owen Dearman <owendearman@gmail.com>
2022-03-03 11:50:19 +0000

    updated CSS stylesheet and image location

--- src/components/PlaceItem.tsx
+++ src/components/PlaceItem.tsx
@@ -11,17 +11,17 @@ interface placeDetails {
 
 function PlaceItem(place: placeDetails): JSX.Element {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h2 style={{ color: "blue" }}>{place.title}</h2>
-      <img src={place.placeImage[0]} alt={place.placeImage[1]} />
+    < div className="contentBlock" >
+      <h2 style={{color: "blue", fontSize: "21px" }}>{place.title}</h2>
+      <img className="image" src={place.placeImage[0]} alt={place.placeImage[1]} />
       <h2>
         {place.name} ({place.country})
       </h2>
       <p>{place.description}</p>
-      <a href={place.locationLink} style={{ textAlign: "left" }}>
+      <a href={place.locationLink}>
         Where is this place?
       </a>
-      <p style={{ textAlign: "right" }}>Times Visited: {place.timesVisited}</p>
+      <p>Times Visited: {place.timesVisited}</p>
     </div>
   );
 }
