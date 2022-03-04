9524ff28dc1372d4980b2b8725155b4202e17d85
Owen Dearman <owendearman@gmail.com>
2022-03-04 12:27:10 +0000

    map Nav bar

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,18 +2,20 @@
 import PlacesInfo from "../PlacesInfo";
 import placesInfoInterface from "../placesInfoInterface";
 
-function addLinks(): JSX.Element{
-  const placeLinks = PlacesInfo.map(makeLink)
-  return <>{placeLinks}</>
+function addLinks(): JSX.Element {
+  const placeLinks = PlacesInfo.map(makeLink);
+  return <>{placeLinks}</>;
 }
 
-function makeLink(place: placesInfoInterface): JSX.Element{
-  return(
-    <div>
-    <a className="LinkHeader" href={place.linkID}>
-    {place.name}
-    </a> |
-    </div>
+function makeLink(place: placesInfoInterface): JSX.Element {
+  const id = "#" + place.linkID;
+  return (
+    <li>
+        <a style={{fontSize: "30px"}} className="LinkHeader" href={id}>
+          {place.name}
+        </a>
+
+    </li>
   );
 }
 
@@ -21,9 +23,7 @@ function PageHeader(): JSX.Element {
   return (
     <header id="pageTop" className="PageTitle">
       <h1 style={{ fontSize: "80px" }}> Owen's Favourite Places</h1>
-      <span style={{ fontSize: "30px" }}>
-        {addLinks()}
-      </span>
+      <span>{addLinks()}</span>
     </header>
   );
 }
