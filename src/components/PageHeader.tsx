74fcf639a5a6424f866b2497f8ff9aed8104498e
Owen Dearman <owendearman@gmail.com>
2022-03-04 12:06:26 +0000

    first attempt map menu items

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,33 +1,28 @@
 //This component concerns the page layout for the header
+import PlacesInfo from "../PlacesInfo";
+import placesInfoInterface from "../placesInfoInterface";
+
+function addLinks(): JSX.Element{
+  const placeLinks = PlacesInfo.map(makeLink)
+  return <>{placeLinks}</>
+}
+
+function makeLink(place: placesInfoInterface): JSX.Element{
+  return(
+    <div>
+    <a className="LinkHeader" href={place.linkID}>
+    {place.name}
+    </a> |
+    </div>
+  );
+}
 
 function PageHeader(): JSX.Element {
   return (
     <header id="pageTop" className="PageTitle">
       <h1 style={{ fontSize: "80px" }}> Owen's Favourite Places</h1>
       <span style={{ fontSize: "30px" }}>
-        <a className="LinkHeader" href="#malhamCove">
-          Malham Cove
-        </a>{" "}
-        |{" "}
-        <a className="LinkHeader" href="#york">
-          York
-        </a>{" "}
-        |{" "}
-        <a className="LinkHeader" href="#cheslyn">
-          Cheslyn Gardens
-        </a>{" "}
-        |{" "}
-        <a className="LinkHeader" href="#porec">
-          Porec
-        </a>{" "}
-        |{" "}
-        <a className="LinkHeader" href="#coch">
-          Cochituate
-        </a>{" "}
-        |{" "}
-        <a className="LinkHeader" href="#jersey">
-          Jersey
-        </a>{" "}
+        {addLinks()}
       </span>
     </header>
   );
